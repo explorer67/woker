ff578865e5ef75e621d7fd6aafe2c8407028a20e
Andrei Piskunov <explorer67@mail.ru>
2021-12-27 16:01:51 +0300

    Initial

---
+++ Main.java
@@ -0,0 +1,10 @@
+package homework_woker;
+
+public class Main {
+    public static void main(String[] args) {
+        Woker.OnTaskDoneListener listener = System.out::println;
+        Woker.OnTaskErrorListener error = System.out::println;
+        Woker woker = new Woker(listener, error);
+        woker.start();
+    }
+}
---
+++ Woker.java
@@ -0,0 +1,30 @@
+package homework_woker;
+
+public class Woker {
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener{
+        void onError(String error);
+    }
+
+    private final OnTaskDoneListener callback;
+    private final OnTaskErrorListener errorCallback;
+
+    public Woker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for(int i = 0; i < 100; i++) {
+            if( i == 32){
+                errorCallback.onError("Task " + i + " is not done");
+            }
+            callback.onDone("Task " + i + " is done");
+        }
+    }
+}
